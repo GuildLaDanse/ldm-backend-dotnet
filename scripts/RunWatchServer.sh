1695cb3be501120b34065c216f5f10b9559efa99
Bavo De Ridder <bavo@bavoderidder.com>
2020-06-09 22:37:34 +0200

    Updated Unix scripts

--- scripts/RunWatchServer.sh
+++ scripts/RunWatchServer.sh
@@ -5,8 +5,6 @@ SCRIPTDIR=`dirname "$BASH_SOURCE"`
 source ${SCRIPTDIR}/Functions.sh
 source ${SCRIPTDIR}/Environment.sh
 
-#cd src/${MAIN_PROJECT}
-
 PrintSectionHeader "Running project with watch"
 
-exec dotnet watch --project src/${MAIN_PROJECT}/${MAIN_PROJECT}.csproj run
+exec dotnet watch -p src/${MAIN_PROJECT}/${MAIN_PROJECT}.csproj run
