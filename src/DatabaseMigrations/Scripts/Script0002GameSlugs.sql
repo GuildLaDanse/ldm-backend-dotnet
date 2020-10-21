c6c89b57301d17e10cd87082e7f2d4f905815a6a
Bavo De Ridder <bavo@bavoderidder.com>
2020-10-21 20:52:17 +0200

    Removed DbUp migrations project

--- src/DatabaseMigrations/Scripts/Script0002GameSlugs.sql
+++
@@ -1,4 +0,0 @@
-﻿ALTER TABLE `GameRealm` ADD `gameSlug` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '';
-
-ALTER TABLE `GameGuild` ADD `gameSlug` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '';
-
