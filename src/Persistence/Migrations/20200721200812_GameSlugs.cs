95a5f5026721e8e772d98ec67c9e4c1ddbd02abd
Bavo De Ridder <bavo@bavoderidder.com>
2020-10-21 20:52:33 +0200

    Added data manipulations for gameSlugs

--- src/Persistence/Migrations/20200721200812_GameSlugs.cs
+++ src/Persistence/Migrations/20200721200812_GameSlugs.cs
@@ -12,19 +12,27 @@ namespace LaDanse.Persistence.Migrations
                 name: "gameSlug",
                 table: "GameRealm",
                 type: "varchar(100)",
-                nullable: true,
+                nullable: false,
                 defaultValue: "")
                 .Annotation("MySql:CharSet", "utf8mb4")
                 .Annotation("MySql:Collation", "utf8mb4_unicode_ci");
+            
+            migrationBuilder.Sql(@"
+                UPDATE GameRealm SET gameSlug = REPLACE(LOWER(name), ' ', '-')
+                ");
 
             migrationBuilder.AddColumn<string>(
                 name: "gameSlug",
                 table: "GameGuild",
                 type: "varchar(100)",
-                nullable: true,
+                nullable: false,
                 defaultValue: "")
                 .Annotation("MySql:CharSet", "utf8mb4")
                 .Annotation("MySql:Collation", "utf8mb4_unicode_ci");
+            
+            migrationBuilder.Sql(@"
+                UPDATE GameGuild SET gameSlug = REPLACE(LOWER(name), ' ', '-')
+                ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
