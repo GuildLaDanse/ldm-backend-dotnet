716973bb5c83ba0bfcc329df2277da4d7fd946de
Bavo De Ridder <bavo@bavoderidder.com>
2020-11-14 23:20:44 +0100

    Generated css is now properly generated and ignored

--- scripts/RunWatchServer.ps1
+++ scripts/RunWatchServer.ps1
@@ -1,6 +1,10 @@
 . $PSScriptRoot/Environment.ps1
 . $PSScriptRoot/Functions.ps1
 
+Write-SectionHeader "Compiling SCSS stylesheets"
+
+sass .\src\WebUI\Styles\app.scss:.\src\WebUI\wwwroot\css\app.css
+
 Write-SectionHeader "Running project with watch"
 
 dotnet watch -p src/WebUI/WebUI.csproj run
