511fd4ab2648e8149d0100b63a414804416ba20a
Bavo De Ridder <bavo@bavoderidder.com>
2020-11-14 18:48:16 +0100

    Ported Calendar page

---
+++ src/WebUI/Models/Calendar/CalendarMonthModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LaDanse.Application.Events.Models;
+
+namespace LaDanse.WebUI.Models.Calendar
+{
+    public class CalendarMonthModel
+    {
+        private readonly DateTime _firstDay;
+        private readonly List<WeekModel> _weeks;
+
+        private static DateTime CalculateFirstDay(DateTime showDay, RaidWeekModel raidWeekModel)
+        {
+            var firstDay = GetClosestMondayBefore(showDay);
+
+            return raidWeekModel.IsInRaidWeek(firstDay) ? firstDay.AddDays(-7) : firstDay;
+        }
+
+        private static DateTime GetClosestMondayBefore(DateTime showDay)
+        {
+            return showDay.DayOfWeek switch
+            {
+                DayOfWeek.Monday => showDay,
+                DayOfWeek.Tuesday => showDay.AddDays(-1),
+                DayOfWeek.Wednesday => showDay.AddDays(-2),
+                DayOfWeek.Thursday => showDay.AddDays(-3),
+                DayOfWeek.Friday => showDay.AddDays(-4),
+                DayOfWeek.Saturday => showDay.AddDays(-5),
+                DayOfWeek.Sunday => showDay.AddDays(-6),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public CalendarMonthModel(DateTime showDay, RaidWeekModel raidWeekModel)
+        {
+            _firstDay = CalculateFirstDay(showDay, raidWeekModel);
+            _weeks = new List<WeekModel>();
+
+            var firstWeekDay = _firstDay;
+
+            for (var i = 0; i < 4; i++)
+            {
+                var weekModel = new WeekModel(firstWeekDay, raidWeekModel);
+
+                _weeks.Add(weekModel);
+
+                firstWeekDay = firstWeekDay.AddDays(7);
+            }
+
+            _weeks.First().Days().First().ShowMonth(true);
+        }
+
+        public DateTime FirstDay()
+        {
+            return _firstDay;
+        }
+
+        public IEnumerable<WeekModel> Weeks()
+        {
+            return _weeks;
+        }
+
+        public DateTime GetStartOfPreviousMonth()
+        {
+            return _firstDay.AddDays(-28);
+        }
+
+        public DateTime GetStartOfNextMonth()
+        {
+            return _firstDay.AddDays(28);
+        }
+
+        public void PopulateEvents(IEnumerable<Event> events)
+        {
+            ResetEvents();
+
+            foreach (var currentEvent in events)
+            {
+                foreach (var currentWeek in _weeks)
+                {
+                    if (currentWeek.IsInWeek(currentEvent.InviteTime.Date))
+                    {
+                        currentWeek.AddEvent(currentEvent);
+                    }
+                }
+            }
+        }
+
+        private void ResetEvents()
+        {
+            foreach (var currentWeek in _weeks)
+            {
+                currentWeek.ResetEvents();
+            }
+        }
+    }
+}
---
+++ src/WebUI/Models/Calendar/RaidWeekModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LaDanse.WebUI.Models.Calendar
+{
+    public class RaidWeekModel
+    {
+        private DateTime _firstDay;
+        private DateTime _lastDay;
+
+        public RaidWeekModel(DateTime showDate) 
+        {
+            Init(showDate);
+        }
+
+        public DateTime FirstDay()
+        {
+            return _firstDay;
+        }
+
+        public DateTime LastDay()
+        {
+            return _lastDay;
+        }
+
+        public bool IsInRaidWeek(DateTime givenDate)
+        {
+            return _firstDay.Date.Equals(givenDate.Date)
+                   || _lastDay.Date.Equals(givenDate.Date)
+                   || (
+                       _firstDay.Date.CompareTo(givenDate.Date) < 0
+                       &&
+                       _lastDay.Date.CompareTo(givenDate.Date) > 0
+                   );
+        }
+
+        private void Init(DateTime showDate) 
+        {
+            var deltaToStart = 0;
+            var deltaToEnd = 0;
+            
+            switch (showDate.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    deltaToStart = 5;
+                    deltaToEnd   = 1;
+                    break;
+                case DayOfWeek.Tuesday: // end of raid week
+                    deltaToStart = 6;
+                    deltaToEnd   = 0;
+                    break;
+                case DayOfWeek.Wednesday: // start of raid week
+                    deltaToStart = 0;
+                    deltaToEnd   = 6;
+                    break;
+                case DayOfWeek.Thursday:
+                    deltaToStart = 1;
+                    deltaToEnd   = 5;
+                    break;
+                case DayOfWeek.Friday:
+                    deltaToStart = 2;
+                    deltaToEnd   = 4;
+                    break;
+                case DayOfWeek.Saturday:
+                    deltaToStart = 3;
+                    deltaToEnd   = 3;
+                    break;
+                case DayOfWeek.Sunday:
+                    deltaToStart = 4;
+                    deltaToEnd   = 2;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            } 
+            
+            _firstDay = showDate.AddDays(-deltaToStart);
+            _lastDay = showDate.AddDays(deltaToEnd);
+        }
+    }
+}
