01f930022795290886595dd90f32188d68de3265
Bavo De Ridder <bavo@bavoderidder.com>
2020-11-28 10:01:03 +0100

    Updated scripts to support Unix environments

--- scripts/Functions.sh
+++ scripts/Functions.sh
@@ -6,10 +6,10 @@ NC='\033[0m' # No Color
 PrintSectionHeader ()
 {
     printf "${NC}\n\n"
-    printf "${SECTIONCOLOR}#############################\n"
+    printf "${SECTIONCOLOR}##################################################\n"
     printf "${SECTIONCOLOR}#\n"
     printf "${SECTIONCOLOR}# ${1}\n"
     printf "${SECTIONCOLOR}#\n"
-    printf "${SECTIONCOLOR}#############################\n"
+    printf "${SECTIONCOLOR}##################################################\n"
     printf "${NC}\n"
 }
