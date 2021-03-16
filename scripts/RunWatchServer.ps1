4f9f750f59d281fef5f17b26ee377347e6baa0fa
Bavo De Ridder <bavo@bavoderidder.com>
2021-03-16 10:50:31 +0100

    Powershell script uses environment variables

--- scripts/RunWatchServer.ps1
+++ scripts/RunWatchServer.ps1
@@ -1,10 +1,6 @@
 . $PSScriptRoot/Environment.ps1
 . $PSScriptRoot/Functions.ps1
 
-Write-SectionHeader "Compiling SCSS stylesheets"
-
-sass .\src\WebUI\Styles\app.scss:.\src\WebUI\wwwroot\css\app.css
-
 Write-SectionHeader "Running project with watch"
 
-dotnet watch -p src/WebUI/WebUI.csproj run
+dotnet watch -p src/$env:MAIN_PROJECT/$env:MAIN_PROJECT.csproj run
