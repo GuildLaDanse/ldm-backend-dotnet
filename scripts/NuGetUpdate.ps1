fcfe083f0b26f51df9a9d34f3c8bd09300c8c497
Bavo De Ridder <bavo@bavoderidder.com>
2021-03-16 10:50:38 +0100

    Removed unused scripts

--- scripts/NuGetUpdate.ps1
+++
@@ -1,6 +0,0 @@
-. $PSScriptRoot/Environment.ps1
-. $PSScriptRoot/Functions.ps1
-
-Write-SectionHeader "Running NuKeeper to update packages"
-
-NuKeeper update -m 10 -c Minor
--- scripts/NuGetUpdate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-NuKeeper update -m 10 -c Minor
